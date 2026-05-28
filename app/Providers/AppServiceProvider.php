394461392263043e67c0264c7c853e9d33384307
Mahmoud Yehia Essa <essa2002135@gmail.com>
2026-05-28 21:44:58 +0300

    for update news6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,46 @@ class AppServiceProvider extends ServiceProvider
         // Register middleware
         // $kernel->appendMiddlewareToGroup('web', CheckUserRole::class);
         
+        // Prevent Symfony Mime from crashing when php_fileinfo is disabled
+        if (class_exists(\Symfony\Component\Mime\MimeTypes::class)) {
+            \Symfony\Component\Mime\MimeTypes::getDefault()->registerGuesser(new class implements \Symfony\Component\Mime\MimeTypeGuesserInterface {
+                public function isGuesserSupported(): bool {
+                    return true;
+                }
+                
+                public function guessMimeType(string $path): ?string {
+                    try {
+                        if (request()) {
+                            $files = request()->allFiles();
+                            $matchingFile = $this->findFileByPath($files, $path);
+                            if ($matchingFile) {
+                                return $matchingFile->getClientMimeType();
+                            }
+                        }
+                    } catch (\Exception $e) {
+                        // Fail silently if request is not available
+                    }
+                    return null;
+                }
+
+                private function findFileByPath($files, $path) {
+                    foreach ($files as $file) {
+                        if (is_array($file)) {
+                            $found = $this->findFileByPath($file, $path);
+                            if ($found) {
+                                return $found;
+                            }
+                        } elseif ($file instanceof \Symfony\Component\HttpFoundation\File\UploadedFile) {
+                            if ($file->getPathname() === $path) {
+                                return $file;
+                            }
+                        }
+                    }
+                    return null;
+                }
+            });
+        }
+        
         view()->composer(['frontend.hmak.body.header', 'frontend.hmak.body.footer'], function ($view) {
             $view->with('newsCategories', \App\Models\NewsCategory::latest()->get());
         });
